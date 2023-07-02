149e28d6959d57ab7b1fb8a982d89140045933c7
Pedro Souza <pedrosport10@gmail.com>
2023-07-02 15:46:05 -0300

    Adicionando arquivos do desafio 02

---
+++ dia02/desafios/desafio02/index.js
@@ -0,0 +1,7 @@
+let value = Number(prompt("Digite o valor do produto: "))
+if(value >= 20){
+    document.getElementById("resultado").innerText = "Aprovado"
+}else{
+    document.getElementById("resultado").innerText = "Reprovado"
+}
+
