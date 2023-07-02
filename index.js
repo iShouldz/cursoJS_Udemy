9c6d2e9693831e49fb88380182ee7b34ae2e2bc8
Pedro Souza <pedrosport10@gmail.com>
2023-07-01 23:06:56 -0300

    Adicionando arquivos curso de JS Udemy

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("JavaScript Funcionando");
+
+let firstname = 'Pedro';
+let lastname = 'Souza';
+console.log(firstname);
+console.log(lastname);
+
+let pen = {
+    itenName: 'Pen',
+    itemPrice: 3,
+    itemAvailable: true,
+    itemColor: "Red"
+}
+
+console.log(pen)
