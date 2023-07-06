547f42b29dcef246f71e66b969232490fbf33d59
Pedro Souza <pedrosport10@gmail.com>
2023-07-06 18:11:08 -0300

    Declarações de função

---
+++ dia05/index.js
@@ -0,0 +1,20 @@
+//Declaração de funcao
+
+function movie(){
+    console.log("The matrix")
+}
+
+movie()
+
+
+//Funcion expression
+
+const car = function(){
+    console.log("Tesla")
+}
+
+car()
+
+const truck = car
+
+truck()
