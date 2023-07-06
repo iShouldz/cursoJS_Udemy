4e478dc1d9980f3a6e8cf8909cb47e23ec86aed1
Pedro Souza <pedrosport10@gmail.com>
2023-07-06 18:16:58 -0300

    Função com argumentos infinitos (dinamico)

--- dia05/index.js
+++ dia05/index.js
@@ -18,3 +18,15 @@ car()
 const truck = car
 
 truck()
+
+function price(){
+    let total = 0;
+    //Permite criar uma função com infinitos argumentos, 
+    //sem ter q declarar, ou seja, uma função dinamica
+    for(let value of arguments){
+        total += value;
+    }
+    return total;
+}
+
+console.log(price(10, 20, 30, 40))
